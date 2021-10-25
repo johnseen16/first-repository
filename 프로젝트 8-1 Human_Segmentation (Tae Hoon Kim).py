e77ecc1792c557e3f5dac3233d4a54f4d2a83e97
johnseen16 <79895378+johnseen16@users.noreply.github.com>
2021-10-25 13:34:09 +0900

    Add files via upload

---
+++ 프로젝트 8-1 Human_Segmentation (Tae Hoon Kim).py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import os
+import cv2
+import numpy as np
+import urllib
+import tarfile
+
+
+from glob import glob
+from matplotlib import pyplot as plt
+import tensorflow as tf
+
+img_path = os.getenv('HOME') + '/aiffel/human_segmentation/images/my_image.png'
+img_orig = cv2.imread(img_path)
+
+
+# In[21]:
+
+
+#segmentation 사람 분리하기!
+class DeepLabModel(object):
+    INPUT_TENSOR_NAME = 'ImageTensor:0'
+    OUTPUT_TENSOR_NAME = 'SemanticPrediction:0'
+    INPUT_SIZE = 513
+    FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+
+# In[22]:
+
+
+def __init__(self, tarball_path):
+       self.graph = tf.Graph()
+       graph_def = None
+       tar_file = tarfile.open(tarball_path)
+
+       for tar_info in tar_file.getmembers():
+           if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+               file_handle = tar_file.extractfile(tar_info)
+               graph_def = tf.compat.v1.GraphDef.FromString(file_handle.read())
+               break
+               
+               tar_file.close()
+
+       with self.graph.as_default():
+           tf.compat.v1.import_graph_def(graph_def, name='')
+
+       self.sess = tf.compat.v1.Session(graph=self.graph)
+
+               
+
+
+# In[27]:
+
+
+class DeepLabModel(object):
+    INPUT_TENSOR_NAME = 'ImageTensor:0'
+    OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+    INPUT_SIZE = 513
+    FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+    # __init__()에서 모델 구조를 직접 구현하는 대신, tar file에서 읽어들인 그래프구조 graph_def를 
+    # tf.compat.v1.import_graph_def를 통해 불러들여 활용하게 됩니다. 
+    def __init__(self, tarball_path):
+        self.graph = tf.Graph()
+        graph_def = None
+        tar_file = tarfile.open(tarball_path)
+        for tar_info in tar_file.getmembers():
+            if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+                file_handle = tar_file.extractfile(tar_info)
+                graph_def = tf.compat.v1.GraphDef.FromString(file_handle.read())
+                break
+        tar_file.close()
+
+        with self.graph.as_default():
+    	    tf.compat.v1.import_graph_def(graph_def, name='')
+
+        self.sess = tf.compat.v1.Session(graph=self.graph)
+
+    # 이미지를 전처리하여 Tensorflow 입력으로 사용 가능한 shape의 Numpy Array로 변환합니다.
+    #이미지를 전처리하여 tf 입력으로 사용가능한 shape의 numpy array로 변환
+    def preprocess(self, img_orig):
+        height, width = img_orig.shape[:2]
+        resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+        target_size = (int(resize_ratio * width), int(resize_ratio * height))
+        resized_image = cv2.resize(img_orig, target_size)
+        resized_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        img_input = resized_rgb
+        return img_input
+     
+    #segmentation 실행!
+    def run(self, image):
+        img_input = self.preprocess(image)
+
+        # Tensorflow V1에서는 model(input) 방식이 아니라 sess.run(feed_dict={input...}) 방식을 활용합니다.
+        batch_seg_map = self.sess.run(
+            self.OUTPUT_TENSOR_NAME,
+            feed_dict={self.INPUT_TENSOR_NAME: [img_input]})
+
+        seg_map = batch_seg_map[0]
+        return cv2.cvtColor(img_input, cv2.COLOR_RGB2BGR), seg_map
+
+
+# In[28]:
+
+
+# define model and download & load pretrained weight
+_DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
+
+model_dir = os.getenv('HOME')+'/aiffel/human_segmentation/models'
+tf.io.gfile.makedirs(model_dir)
+
+print ('temp directory:', model_dir)
+
+download_path = os.path.join(model_dir, 'deeplab_model.tar.gz')
+if not os.path.exists(download_path):
+    urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + 'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+                   download_path)
+
+MODEL = DeepLabModel(download_path)
+print('model loaded successfully!')
+
+
+# In[29]:
+
+
+img_resized, seg_map = MODEL.run(img_orig)
+print (img_orig.shape, img_resized.shape, seg_map.max())
+
+
+# In[30]:
+
+
+LABEL_NAMES = [
+    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+]
+len(LABEL_NAMES)
+
+
+# In[31]:
+
+
+#segmentation 결과 원래 SIZE로 받기!
+img_show = img_resized.copy()
+seg_map = np.where(seg_map == 15, 15, 0) # 예측 중 사람만 추출
+img_mask = seg_map * (255/seg_map.max()) # 255 normalization
+img_mask = img_mask.astype(np.uint8)
+color_mask = cv2.applyColorMap(img_mask, cv2.COLORMAP_JET)
+img_show = cv2.addWeighted(img_show, 0.6, color_mask, 0.35, 0.0)
+
+plt.imshow(cv2.cvtColor(img_show, cv2.COLOR_BGR2RGB))
+plt.show()
+
+
+# In[32]:
+
+
+img_mask_up = cv2.resize(img_mask, img_orig.shape[:2][::-1], interpolation=cv2.INTER_LINEAR)
+_, img_mask_up = cv2.threshold(img_mask_up, 128, 255, cv2.THRESH_BINARY)
+
+ax = plt.subplot(1, 2, 1)
+plt.imshow(img_mask_up, cmap=plt.cm.binary_r)
+ax.set_title('Original Size Mask')
+
+ax = plt.subplot(1, 2, 2)
+plt.imshow(img_mask, cmap=plt.cm.binary_r)
+ax.set_title('DeepLab Model Mask')
+
+plt.show()
+
+
+# In[33]:
+
+
+#배경화면 받기!
+img_mask_color = cv2.cvtColor(img_mask_up, cv2.COLOR_GRAY2BGR)
+img_bg_mask = cv2.bitwise_not(img_mask_color)    # 이미지 반전 - 배경 255, 사람 0
+img_bg = cv2.bitwise_and(img_orig, img_bg_mask)  # 배경만 있는 영상 얻기
+plt.imshow(img_bg)
+plt.show()
+
+# 배경 흐리게 하기
+img_bg_blur = cv2.blur(img_bg, (13, 13))
+plt.imshow(cv2.cvtColor(img_bg_blur, cv2.COLOR_BGR2RGB))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
